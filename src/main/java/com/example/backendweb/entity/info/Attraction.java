f6a8718b5ab0281a2e024e08fa9bfd5279aa0867
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-01-24 22:02:40 +0800

    Completed Attraction in Information module

---
+++ src/main/java/com/example/backendweb/entity/info/Attraction.java
@@ -0,0 +1,51 @@
+package com.example.backendweb.entity.info;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.Map;
+
+/**
+ * @ClassName Attraction
+ * @Description
+ * @Author HUANG ZHENJIA
+ * @StudentID A0298312B
+ * @Date 2025/1/24
+ * @Version 1.0
+ */
+
+@Entity
+@Table(name = "attractions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Attraction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer attractionId; // 景点唯一标识
+
+    @Column(nullable = false, length = 255)
+    private String attractionName; // 景点名称
+
+    @Column(nullable = false, length = 255)
+    private String location; // 景点位置
+
+    @Column(columnDefinition = "TEXT")
+    private String description; // 景点描述
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json", nullable = false)
+    private Map<String, Double> ticketType; // 门票类型及价格
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json", nullable = false)
+    private Map<String, String> openingHours; // 开放时间
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json", nullable = false)
+    private Map<String, Integer> ticketAvailability; // 各类型票的剩余数量
+}
