963a94ee07e5a813888ea6be92aed4721bf58a4d
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-02-06 14:15:42 +0800

    Move AttractionBookingRepository.java

---
+++ src/main/java/com/example/backendweb/Repository/Booking/AttractionBookingRepository.java
@@ -0,0 +1,18 @@
+package com.example.backendweb.Repository.Booking;
+
+import com.example.backendweb.Entity.Booking.AttractionBooking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @ClassName AttractionBookingRepository
+ * @Description
+ * @Author HUANG ZHENJIA
+ * @StudentID A0298312B
+ * @Date 2025/2/6
+ * @Version 1.0
+ */
+
+@Repository
+public interface AttractionBookingRepository extends JpaRepository<AttractionBooking, Integer> {
+}
