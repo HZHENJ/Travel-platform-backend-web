2cb2bee6e26a822a8449cd8dc4b46f48e0c3b8ad
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-02-06 18:57:00 +0800

    Modify .

--- src/main/java/com/example/backendweb/Repository/User/AuthenticationRepository.java
+++ src/main/java/com/example/backendweb/Repository/User/AuthenticationRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface AuthenticationRepository extends JpaRepository<Authentication,Integer> {
     Optional<Authentication> findByUsername(String username);
+    Optional<Authentication> findByUserEmail(String email);
 }
