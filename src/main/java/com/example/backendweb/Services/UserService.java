f97d85a91cd4c703cbf6e03d1f2e84cc02b56b7c
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-02-06 16:02:06 +0800

    Modify sth

--- src/main/java/com/example/backendweb/Services/UserService.java
+++ src/main/java/com/example/backendweb/Services/UserService.java
@@ -29,8 +29,9 @@ public class UserService {
     }
 
     //
-    public User Login(String Username, String Password){
+    public User login(String Username, String Password){
         Optional<Authentication> data = authenticationRepository.findByUsername(Username);
+        // if (data.isPresent() && bCryptPasswordEncoder.matches(password, data.get().getPasswordHash()))
         if(data.isPresent() && data.get().getPasswordHash().equals(Password)){
             return data.get().getUser();
         }else{
@@ -39,24 +40,32 @@ public class UserService {
     }
 
     //
-    public boolean RetriveData(Authentication auth, User user, Preference pre){
+    public boolean registerUserData(Authentication auth, User user, Preference pre){
         return registerUser(user) && registerAuthentication(auth,user) && registerPreference(pre,user);
     }
 
-    //
-    public boolean registerUser(User user){
-        if(user == null) throw new CustomException("Empty Data",400);
-        Optional<User> existingUser = userRepository.findById(user.getUserId());
-        if(existingUser.isPresent()) throw new CustomException("User already exits",409);
+    /**
+     * register User
+     */
+    public boolean registerUser(User user) {
+        if (user == null) throw new CustomException("Empty Data", 400);
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new CustomException("User already exists", 409);
+        }
         userRepository.save(user);
         return true;
     }
 
-    //
-    public boolean registerAuthentication(Authentication auth,User user){
-        if(auth == null || user == null) throw new CustomException("Empty Data",400);
+    public boolean registerAuthentication(Authentication auth, User user){
+        if(auth == null || user == null) {
+            throw new CustomException("Empty Data",400);
+        }
         Optional<Authentication> existingAuth = authenticationRepository.findById(auth.getAuthId());
-        if(existingAuth.isPresent()) throw new CustomException("User already exits",409);
+
+        if(existingAuth.isPresent()) {
+            throw new CustomException("User already exits",409);
+        }
+
         auth.setPasswordHash(bCryptPasswordEncoder.encode(auth.getPasswordHash()));
         auth.setUser(user);
         authenticationRepository.save(auth);
