c7a91c125ce51ebd20aa5cd210850bfbf091204c
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-02-10 12:16:21 +0800

    Add findByAttractionId

--- src/main/java/com/example/backendweb/Repository/Review/ReviewRepository.java
+++ src/main/java/com/example/backendweb/Repository/Review/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.example.backendweb.Repository.Review;
 
 import com.example.backendweb.Entity.Review.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,4 +19,7 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
     List<Review> findByUserId(Integer userId);
     List<Review> findByItemIdAndItemType(Integer itemId, Review.ItemType itemType);
     boolean existsByUserIdAndItemId(Integer userId, Integer itemId);
+
+    @Query("SELECT r FROM Review r WHERE r.itemId = :itemId AND r.itemType = 'Attraction'")
+    List<Review> findByAttractionId(@Param("itemId") Integer itemId);
 }
