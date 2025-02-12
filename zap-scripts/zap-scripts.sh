091d7aff255d406611ed1acec607170307e2e578
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-02-12 22:03:40 +0800

    Dast ~

--- zap-scripts/zap-scripts.sh
+++ zap-scripts/zap-scripts.sh
@@ -2,6 +2,6 @@
 # Pull the latest ZAP Docker image
 docker pull zaproxy/zap-stable
 # Run ZAP baseline scan on the target application and generate report
-docker run -i zaproxy/zap-stable zap-baseline.py -t "http://http://18.143.182.252:8080" - 1 PASS > zap_baseline_report.html
+docker run -i zaproxy/zap-stable zap-baseline.py -t "http://18.143.182.252:8080" - 1 PASS > zap_baseline_report.html
 # Exit without printing any error message
 echo $? › / dev/null
